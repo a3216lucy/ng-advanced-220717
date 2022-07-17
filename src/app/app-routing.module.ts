421f93a99be62dbfa050729376b0061a5808cac9
kimi <kimi.lai@tpisoftware.com>
2022-07-17 11:36:51 +0800

    feat: 完成layout 設定與 login 頁面拆分

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { ColorsComponent } from './utilities/colors/colors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        title: 'SB Admin 2 - Dashboard',
+      },
+      { path: 'page1', component: Page1Component, title: 'SB Admin 2 - Page1' },
+      { path: 'page2', component: Page2Component, title: 'SB Admin 2 - Page2' },
+      {
+        path: 'utilities',
+        children: [{ path: 'colors', component: ColorsComponent }],
+      },
+    ],
+  },
+  // { path: '**', pathMatch: 'full', redirectTo: '/dashboard' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
